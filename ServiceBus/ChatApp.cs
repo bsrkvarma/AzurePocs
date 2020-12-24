361456160971e2d150d92f50a3d4e4db9ac98d10
varma <bsrkvarma@hotmail.com>
2020-12-23 19:06:50 -0500

    Added comments

--- ServiceBus/ChatApp.cs
+++ ServiceBus/ChatApp.cs
@@ -2,10 +2,14 @@ class Program
     {
         static void Main(string[] args)
         {
+            //Service bus connection string
             var serviceBusConnectionString = "";
+            //provide valid topic name
             var topicPath = "ChatTopic";
-
+            
+            //Management Clinet to interact with service bus
             var manager = new ManagementClient(serviceBusConnectionString);
+            //Create a topic to hold the subscription
             if (!manager.TopicExistsAsync(topicPath).Result)
             {
                 manager.CreateTopicAsync(topicPath).Wait();
@@ -17,7 +21,8 @@ class Program
             {
                 AutoDeleteOnIdle = TimeSpan.FromDays(7)
             };
-
+            
+            //Create a subscription for the topic
             if (!manager.SubscriptionExistsAsync(topicPath, description).Result)
             {
                 var subscriptionCreateRes = manager.CreateSubscriptionAsync(topicDescription);
@@ -34,23 +39,29 @@ class Program
             var messageResult = topicClient.SendAsync(message);
             messageResult.Wait();
 
+            //Subscription handler to receive the 
             var subClient = new SubscriptionClient(serviceBusConnectionString, topicPath, description);
             subClient.RegisterMessageHandler(ProcessMessagesAsync, Exceptionhandler);
+            
+            //Listen to the console
             while (true)
             {
                 var chat = Console.ReadLine();
                 if (chat == "exit")
                     break;
+                //Post the chat message to the subscription
                 var data = JsonConvert.SerializeObject(new { message = chat });
                 var chatMessage = new Message(Encoding.UTF8.GetBytes(data)) { Label = description };
                 topicClient.SendAsync(chatMessage).Wait();
             }
-            //Close the clients
+            
+            //Dispose the clients
             topicClient.CloseAsync().Wait();
             subClient.CloseAsync().Wait();
             manager.CloseAsync().Wait();
         }
-
+          
+        //Process the message
         private async static Task ProcessMessagesAsync(Message msg, CancellationToken arg2)
         {
             var text = Encoding.UTF8.GetString(msg.Body);
@@ -60,6 +71,7 @@ class Program
 
         private static Task Exceptionhandler(ExceptionReceivedEventArgs arg)
         {
-            throw new NotImplementedException();
+                Debug.WriteLine(arg.Exception);
+                throw new NotImplementedException();
         }
     }
